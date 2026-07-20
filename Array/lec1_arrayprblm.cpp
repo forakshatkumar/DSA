73d62737fb332d7fdc1c1d11e0c3a2b8d6a16b60
Akshat Kumar <forakshatkumar@gmail.com>
2026-07-20 14:20:42 +0530

    New Problem

--- Array/lec1_arrayprblm.cpp
+++ Array/lec1_arrayprblm.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     // your code goes here
     int A[10];
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         cin >> A[i];
     }
-    int max = A[0],min = A[0], sum = 0;
-    for (int i = 0; i < 10; i++){
+    int max = A[0], min = A[0], sum = 0, max_index, min_index;
+    for (int i = 0; i < 10; i++)
+    {
         sum += A[i];
-        if(A[i] > max){
+        if (A[i] > max)
+        {
             max = A[i];
+            max_index = i;
         }
-        else{
+        else
+        {
             min = A[i];
+            min_index = i;
         }
     }
-    for (int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++)
+    {
         cout << &A[i] << endl; // f0r the address
     }
-    
-    cout <<"max value : "<< max << endl;
-    cout << "min value : "<<min << endl;
-    cout << "sum of array : "<<sum << endl;
+
+    cout << "max value : " << max << "index is : " << max_index << endl;
+    cout << "min value : " << min << "inex is : " << min_index << endl;
+    cout << "sum of array : " << sum << endl;
     cout << &max << endl;
     cout << &min << endl;
 }
