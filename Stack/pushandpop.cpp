febfcd5edb97e476a8a323aa5e0f3ff2fabd3f24
Akshat Kumar <forakshatkumar@gmail.com>
2026-07-23 16:02:57 +0530

    Updated files

---
+++ Stack/pushandpop.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1000;
+int s[MAX];
+int top = -1;
+int n;
+
+void push(int a) {
+    if (top == n - 1) {
+        cout << "Overflow" << endl;
+    } else {
+        top++;
+        s[top] = a;
+    }
+}
+
+void pop() {
+    if (top == -1) {
+        cout << "Underflow" << endl;
+    } else {
+        cout << "Item popped: " << s[top] << endl;
+        top--;
+    }
+}
+
+void display() {
+    if (top == -1) {
+        cout << "Stack is empty" << endl;
+        return;
+    }
+
+    cout << "Stack elements: ";
+    for (int i = 0; i <= top; i++) {
+        cout << s[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        push(x);
+    }
+
+    display();
+    for (int i = 0; i < n; i++) {
+        pop();
+    }
+    display();
+
+    return 0;
+}
